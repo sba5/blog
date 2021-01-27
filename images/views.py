2bbf1feddbe774f49ef2749a03a35280ffa950c1
kyuhyeon <narvik37@naver.com>
2021-01-28 00:12:44 +0900

    이미지 업로드 구현중

--- images/views.py
+++ images/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
-
+from .models import Photo
 
 # Create your views here.
 
 def photo(req):
+    if req.method == 'POST':
+        title = req.POST.get('imgtitle')
+        print('============')
+        print(req)
+        print(title)
+
     return render(req, 'photo.html') 
