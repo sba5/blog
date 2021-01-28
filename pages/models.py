ac6312dc46af6a218902ef1879e0c1b4b2c32140
kyuhyeon <narvik37@naver.com>
2021-01-28 22:17:20 +0900

    다이어리만드는중

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Diary(models.Model):
+    id = models.AutoField(primary_key=True)
+    content = models.CharField(max_length=16, verbose_name="user_id")
+    
+    class Meta:
+        db_table = "Diaries"
+        verbose_name = "Diaries"
+        verbose_name_plural = "Diaries"
+
+    def __str__(self):
+        return self.content
--- pages/views.py
+++ pages/views.py
@@ -30,4 +30,48 @@ def index(req, res_data):
     return render(req, 'index.html', res_data)
 
 def diary(req):
-    return render(req, 'diary.html')
+    if req.method == 'POST':
+        diary = req.POST.get('diary')
+
+        diary = Diary(
+            diary = diary,
+        )
+        diary.save()
+
+        res_data = {}
+        diaries = Diary.objects.all().order_by('-id')
+        diarylist = []
+        for diary in diaries:
+            diarylist.append(diary.diary)
+             
+        for i in range(0, 10):
+            res_data[f'diary{i}'] = str(diarylist[i])
+
+        # res_data = {}
+        # # res_data = {}
+        # # res_data['res'] = '회원가입을 축하드립니다!'
+        # # print(req.POST['username'])
+        # res_data['title0'] = title
+        # print(str(imgs))
+        # print(title)
+        # res_data['img0'] = 'http://27.96.131.103:8000/media/images/'+str(imgs)
+        return render(req, 'photo.html', res_data)
+
+
+    diary = req.POST.get('diary')
+
+    diary = Diary(
+        diary = diary,
+    )
+    diary.save()
+
+    res_data = {}
+    diaries = Diary.objects.all().order_by('-id')
+    diarylist = []
+    for diary in diaries:
+        diarylist.append(diary.diary)
+             
+    for i in range(0, 10):
+        res_data[f'diary{i}'] = str(diarylist[i])
+    
+    return render(req, 'photo.html', res_data) 
